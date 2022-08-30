574ef8b6935380084b65c8abfd2c7fd83f5c65a9
macwdo <danilo.macedofernandes@hotmail.com>
2022-08-30 12:54:07 -0300

    rechng

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/danilo/Estudos/basic-crud-django/venv"
+set -gx VIRTUAL_ENV "/home/danilo/Estudos/my-project-django/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
